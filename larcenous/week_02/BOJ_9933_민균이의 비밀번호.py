fde7b45469f642b230a09b13ad8314be62a2044c
Geonwoo, Shin <matt0621@korea.ac.kr>
2023-12-11 18:20:22 +0900

    refactor: BOJ_9933_민균이의 비밀번호

--- larcenous/week_02/BOJ_9933_민균이의 비밀번호.py
+++ larcenous/week_02/BOJ_9933_민균이의 비밀번호.py
@@ -3,6 +3,6 @@ dic = dict()
 for word in [input() for _ in range(N)] :
     rword = ''.join(reversed(list(word))) #역순으로 된 비밀번호
     if word in dic.values() or word == rword:
-        print(len(word),list(word)[len(word)//2])
+        print(len(word),word[len(word)//2])
         break
     dic[word] = rword
