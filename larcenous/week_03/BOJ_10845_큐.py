c624077ed29d7a4008b8244da9f44cffc28258e5
Geonwoo, Shin <matt0621@korea.ac.kr>
2023-12-23 18:03:25 +0900

    add: BOJ_10845_큐

---
+++ larcenous/week_03/BOJ_10845_큐.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+queue = deque()
+
+for _ in range(N) :
+    order = input().rstrip().split()
+    if order[0] == 'push' :
+        queue.append(int(order[1]))
+    elif order[0] == 'pop' :
+        if len(queue) == 0 :
+            print(-1)
+        else :
+            print(queue.popleft())
+    elif order[0] == 'size' :
+        print(len(queue))
+    elif order[0] == 'empty' :
+        if len(queue) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif order[0] == 'front' :
+        if len(queue) == 0 :
+            print(-1)
+        else :
+            print(queue[0])
+    elif order[0] == 'back' :
+        if len(queue) == 0 :
+            print(-1)
+        else :
+            print(queue[-1])
