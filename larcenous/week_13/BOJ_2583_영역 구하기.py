ee51aebfb43e690ef309a9239511313985385010
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-03-05 14:14:20 +0900

    refactor: BOJ_2583_영역 구하기.py

--- larcenous/week_13/BOJ_2583_영역 구하기.py
+++ larcenous/week_13/BOJ_2583_영역 구하기.py
@@ -1,5 +1,8 @@
+#for문 범위 설정에 유의
 from collections import deque
-M, N, K = map(int,input().split()) #M이 열, N이 행
+import sys
+input = sys.stdin.readline
+M, N, K = map(int,input().strip().split()) #M이 열, N이 행
 visited = [[False]*M for _ in range(N)]
 cnt = 0
 ans = []
@@ -14,22 +17,22 @@ def BFS(x,y) :
         for ddx,ddy in zip(dx,dy) :
             nx, ny = cx+ddx, cy+ddy
             if 0<=nx<M and 0<=ny<N :
-                visited[ny][nx] = True
-                q.append((nx,ny))
-                area += 1
+                if not visited[ny][nx] :
+                    visited[ny][nx] = True
+                    q.append((nx,ny))
+                    area += 1
     return area
 
 for _ in range(K) :
-    x,y,x_,y_ = map(int,input().split()) #x가 열
-    for i in range(x,x_+1) :
-        for j in range(y,y_+1) :
-            print(j,i)
-            visited[j][i] = True
-print(visited)
+    x,y,x_,y_ = map(int,input().strip().split()) #x가 열
+    for i in range(x,x_) :
+        for j in range(y,y_) :
+            visited[i][j] = True
+
 for i in range(M) :
     for j in range(N) :
         if not visited[j][i] :
             cnt += 1
             ans.append(BFS(i,j))
 print(cnt)
-print(*ans)
+print(*sorted(ans))
