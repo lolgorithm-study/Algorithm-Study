445eb33dc72748f8ad303926dc375be0088a2a41
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-03-13 00:23:04 +0900

    refactor: BOJ_5719_거의최단경로.py

--- larcenous/week_14/BOJ_5719_거의최단경로.py
+++ larcenous/week_14/BOJ_5719_거의최단경로.py
@@ -1,30 +1,48 @@
 import sys
 from heapq import *
 from collections import *
-#input = sys.stdin.readline
+input = sys.stdin.readline
 
-def Dijkstra(start,end,graph,N,shortest_path=None) :
+def Dijkstra(start,end,graph,N) :
+    minload = [[] for _ in range(N)]
     INF = sys.maxsize
-    q = [(0,start,())]
+    q = [(0,start)]
     visited = set()
-    if shortest_path != None :
-        visited = set(shortest_path).difference({start,end})
     D = [INF]*N
     while q :
-        cost, current, path = heappop(q)
+        cost, current = heappop(q)
         if current not in visited :
             visited.add(current)
             if current == end :
-                return (cost,path)
+                return cost, minload
             for edge_weight, next in graph.get(current,()):
                 if next in visited :
                     continue
                 cum_cost = cost + edge_weight
                 if cum_cost < D[next] :
                     D[next] = cum_cost
-                    path = (path, next)
-                    heappush(q, (cum_cost,next,path))
-    return -1
+                    minload[next].clear()
+                    minload[next].append(current) #최단 경로를 역방향으로 생성
+                    heappush(q, (cum_cost,next))
+                #최단 경로가 두 개 이상 존재할 수 있음
+                elif cum_cost == D[next] : 
+                    minload[next].append(current)
+    return -1, minload #minload가 아닌 []로 했다가 BFS를 돌리는 과정에서 IndexError가 발생
+
+def BFS(start, graph, minload) : #최단 경로를 역추적하며, 가중치를 무한대로 변경하여 사용할 수 없게 만듬
+    INF = sys.maxsize
+    q = deque([start])
+    visited = set([start])
+    while q :
+        current = q.popleft()
+        for next in minload[current] :
+            if next not in visited:
+                q.append(next)
+                visited.add(next)
+            for i in range(len(graph[next])) :
+                if graph[next][i][1] == current :
+                    graph[next][i][0] = INF
+    return graph
 
 while True :
     N, M = map(int,input().split())
@@ -34,6 +52,8 @@ while True :
     graph = defaultdict(list)
     for _ in range(M) :
         U, V, P = map(int,input().split())
-        graph[U].append((P,V))
-    cost, path = Dijkstra(S,D,graph,N)
-    print(Dijkstra(S,D,graph,N,path)[0])
+        graph[U].append([P,V])
+    cost, minload = Dijkstra(S,D,graph,N)
+    graph = BFS(D, graph, minload)
+    cost, minload = Dijkstra(S,D,graph,N)
+    print(cost)
